32a5cf4115d5705efc04ceb5e0a167830bc6039a
kartik <kartikmendu@gmail.com>
2024-08-31 16:01:17 +0530

    Hosting frontend from express.

--- src/index.ts
+++ src/index.ts
@@ -5,15 +5,23 @@ import clientRouter from "./routes/client.routes";
 import cors from "cors";
 import userRouter from "./routes/user.routes";
 import operationRouter from "./routes/operation.routes";
+import path from "path";
 const app = express();
 
 app.use(express.json(), cors({ origin: "*" }));
 
-app.use("/", authRouter, operationRouter);
-app.use("/client", clientRouter);
-app.use("/user", userRouter);
+app.use("/api", authRouter, operationRouter);
+app.use("/api/client", clientRouter);
+app.use("/api/user", userRouter);
 
 export const prisma = new PrismaClient();
+
+// --------------- Hosting frontend ---------------
+const dir = path.resolve();
+app.use(express.static(path.join(dir, "dist")));
+app.get("*", (req, res) =>
+  res.sendFile(path.resolve(dir, "dist", "index.html"))
+);
 
 app.listen(4500, () => {
   console.log("App listening on port 4500!");
